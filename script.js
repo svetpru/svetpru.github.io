8b787858d3349b7b5041e0c460468a27241eb2ec
svetpru <sv.prud@gmail.com>
2020-11-29 14:49:01 +0300

    Upset

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ $(document).ready(function () {
     $("[data-toggle='tooltip']").tooltip();
 
     $("[data-toggle='popover']").popover();
+
+    //Формируем запрос
+    $(".modal-body").load("https://sashapozharin.github.io img.d-block.w-100", function (response, status, request) {
+        
+    });
+
+});
+
+$("#btnSelect").click(function (e) { 
+    console.log($("option[selected='true']"))
+    
 });
