d782c413bd60db7984fbb29395ec11b14e337de8
svetpru <sv.prud@gmail.com>
2020-12-06 13:08:09 +0300

    upset

--- script.js
+++ script.js
@@ -11,10 +11,14 @@ $("#btnSelect").click(function (e) {
     //переменные
     let select = document.querySelector("select.custom-select")
     let url = select.options[select.selectedIndex].value + " " + "div#answer";
+
+
+    document.querySelector("#msgLabel").innerHTML = "User: " + select.options[select.selectedIndex].innerHTML;
     
     //Формируем запрос
     $(".modal-body").load(url, function (response, status, request) {
         //Убрать аттрибут id
         document.querySelector(".modal-body>div").id="";
+        $("#msgBox").modal("show")
     });
 });
